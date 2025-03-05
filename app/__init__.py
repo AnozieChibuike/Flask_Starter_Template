15aec9e1d873dd50cf7a487cdf1905ac0eefa294
AnozieChibuike <chibuikeanozie0@gmail.com>
2025-03-05 17:41:31 +0100

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from config import Config
+from flask_migrate import Migrate # type: ignore[import-untyped]
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flasksqlalchemybasemodel import db
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+migrate = Migrate(app,db)
+jwt = JWTManager(app)
+
+from app import models
+from app import routes
